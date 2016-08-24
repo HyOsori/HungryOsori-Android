da051814b2514b86e8223c0b3a34de0679ef7e71
GunjuKo <rhrjswn2@naver.com>
2016-08-25 03:22:37 +0900

    fragment 구현

---
+++ app/src/main/java/android/webcrawler/osori/hungryosori/Fragment/CrawlerFragment.java
@@ -0,0 +1,63 @@
+package android.webcrawler.osori.hungryosori.Fragment;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webcrawler.osori.hungryosori.Adapter.CrawlerListAdapter;
+import android.webcrawler.osori.hungryosori.R;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.ToggleButton;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kunju on 2016-08-25.
+ */
+public class CrawlerFragment extends Fragment{
+
+    private int page;
+    private ListView listView;
+    private CrawlerListAdapter listAdapter;
+
+    public static CrawlerFragment newInstance(int pagePosition){
+        CrawlerFragment fragment = new CrawlerFragment();
+
+        Bundle bundle = new Bundle();
+        bundle.putInt("PAGE", pagePosition);
+        fragment.setArguments(bundle);
+
+        return fragment;
+    }
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        Bundle args = getArguments();
+        if(args != null){
+            page      = args.getInt("PAGE");
+        }
+
+        if(page == 0) {
+            listAdapter = new CrawlerListAdapter(getActivity(), R.layout.list_crawler, R.id.list_crawler_textView_title, null);
+        }else if(page == 1){
+            listAdapter = new CrawlerListAdapter(getActivity(), R.layout.list_crawler, R.id.list_crawler_textView_title, null);
+        }
+    }
+
+    // 프레그먼트의 뷰를 생성한다
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_crawler, container, false);
+
+        listView = (ListView)view.findViewById(R.id.fragment_crawler_listView);
+        listView.setAdapter(listAdapter);                       // 어댑터 설정
+
+        return view;
+    }
+
+}
