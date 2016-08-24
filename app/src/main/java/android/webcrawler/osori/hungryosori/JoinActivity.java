4a0623ce27b1ee5a4c1c57bc4ec698e3cdeae168
GunjuKo <rhrjswn2@naver.com>
2016-08-25 01:51:45 +0900

    회원 가입 페이지 구현 완료

--- app/src/main/java/android/webcrawler/osori/hungryosori/JoinActivity.java
+++ app/src/main/java/android/webcrawler/osori/hungryosori/JoinActivity.java
@@ -1,6 +1,5 @@
 package android.webcrawler.osori.hungryosori;
 
-import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -9,42 +8,46 @@ import android.webcrawler.osori.hungryosori.common.Constant;
 import android.webcrawler.osori.hungryosori.common.Lib;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
+
 
 /**
  * Created by 고건주&김규민 on 2016-08-18.
- * 로그인 페이지 액티비티
+ * 회원가입 페이지 액티비티
  */
 
-public class LoginActivity extends FragmentActivity {
+public class JoinActivity extends FragmentActivity {
     private static String email;
     private static String password;
-    private EditText editText_mail, editText_password;
+    private static String rePassword;
 
+    private EditText editText_mail, editText_password, editText_repassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_join);
 
         /** 객체 설정 */
-        editText_mail       = (EditText) findViewById(R.id.login_editText_email);
-        editText_password   = (EditText) findViewById(R.id.login_editText_password);
+        editText_mail = (EditText) findViewById(R.id.join_editText_email);
+        editText_password = (EditText) findViewById(R.id.join_editText_password);
+        editText_repassword = (EditText) findViewById(R.id.join_editText_repassword);
 
         /** 폰트 설정 */
         Typeface fontArial = Typeface.createFromAsset(getAssets(), "fonts/arial.ttf");
         editText_mail.setTypeface(fontArial);
         editText_password.setTypeface(fontArial);
-        ((TextView)findViewById(R.id.login_button_join)).setTypeface(fontArial);
-        ((Button)findViewById(R.id.login_button_login)).setTypeface(fontArial);
+        editText_repassword.setTypeface(fontArial);
+        ((Button)findViewById(R.id.join_button_submit)).setTypeface(fontArial);
     }
 
     public void onClick(View v) {
 
         switch (v.getId()) {
-            case R.id.login_button_login:
+
+            case R.id.join_button_submit:
                 email = editText_mail.getText().toString().trim();
                 password = editText_password.getText().toString().trim();
+                rePassword = editText_repassword.getText().toString().trim();
 
                 /** 아이디가 올바른 이메일 형식인지 검사 */
                 if (!Lib.isValidEmail(email)) {
@@ -54,18 +57,19 @@ public class LoginActivity extends FragmentActivity {
                 }
 
                 /** 패스워드가 최소 6자리를 넘었는지를 검사 */
-               if (password.length() < Constant.PASSWORD_LENGTH_MIN) {
+                if (password.length() < Constant.PASSWORD_LENGTH_MIN) {
                     Toast.makeText(this, "패스워드는 6자리 이상 입력해주세요.", Toast.LENGTH_LONG).show();
                     editText_password.requestFocus();
                     break;
                 }
+
+                /** 비밀번호 확인 */
+                if(!password.equals(rePassword)){
+                    Toast.makeText(this,"비밀번호 확인 틀렸습니다.", Toast.LENGTH_SHORT).show();
+                    break;
+                }
                 /** 서버 연동 */
 
-                break;
-
-            case R.id.login_button_join:
-                Intent intent = new Intent(this,JoinActivity.class);
-                startActivity(intent);
                 break;
         }
     }
