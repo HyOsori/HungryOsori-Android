a244b0e3b21089d4a90dde199efe4b700fb9bc2b
gunjuKo <rhrjswn2@naver.com>
2016-12-30 16:40:02 +0900

    Method 추상 클래스 수정

--- app/src/main/java/android/webcrawler/osori/hungryosori/Interface/Method.java
+++ app/src/main/java/android/webcrawler/osori/hungryosori/Interface/Method.java
@@ -1,94 +1,9 @@
 package android.webcrawler.osori.hungryosori.Interface;
-
-import android.webcrawler.osori.hungryosori.Common.Constant;
-import android.webcrawler.osori.hungryosori.Common.HttpResult;
 import android.webcrawler.osori.hungryosori.Model.ParamModel;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Map;
 
 /**
  * Created by kunju on 2016-12-28.
  */
 public abstract class Method {
-    private     boolean cookieSet;
-    private     boolean cookieGet;
-
-    public boolean isCookieGet() {
-        return cookieGet;
-    }
-
-    public void setCookieGet(boolean cookieGet) {
-        this.cookieGet = cookieGet;
-    }
-
-    protected   HttpURLConnection urlConnection;
-
-    public Method(){
-        cookieSet = false;
-    }
-
-    public boolean isCookieSet() {
-        return cookieSet;
-    }
-
-    public void setCookieSet(boolean cookieSet) {
-        this.cookieSet = cookieSet;
-    }
-
-    public abstract HttpResult send(ParamModel paramModel);
-
-    protected void setCookie(){
-        if (isCookieSet()) {
-            // urlConnection.setRequestProperty("cookie", Constant.cookie);
-        }
-    }
-
-    protected String readResponseMessage(){
-        StringBuilder strBuilder = new StringBuilder();
-        InputStream in          = null;
-        BufferedReader bReader  = null;
-        try {
-            in = new BufferedInputStream(urlConnection.getInputStream());
-            bReader = new BufferedReader(new InputStreamReader(in, Constant.DEFAULT_ENCODING));
-
-            String line;
-            while ((line = bReader.readLine()) != null) {
-                strBuilder.append(line);
-            }
-        }catch (IOException e){
-
-        }finally {
-            if(in != null){
-                try {
-                    in.close();
-                    if(bReader != null)
-                        bReader.close();
-                }catch (IOException e){
-
-                }
-            }
-        }
-
-        return strBuilder.toString();
-    }
-    protected String readCookie(){
-        /** 쿠키 값 읽어오기 */
-        StringBuilder cookie = new StringBuilder();
-        Map map = urlConnection.getHeaderFields();
-        if (map != null && map.containsKey("Set-Cookie")) {
-            Collection c = (Collection) map.get("Set-Cookie");
-            for (Iterator i = c.iterator(); i.hasNext(); ) {
-                cookie.append(i.next() + ", ");
-            }
-        }
-        return cookie.toString();
-    }
+    public abstract String send(ParamModel paramModel);
 }
--- app/src/main/java/android/webcrawler/osori/hungryosori/common/Constant.java
+++ app/src/main/java/android/webcrawler/osori/hungryosori/common/Constant.java
@@ -18,15 +18,12 @@ public class Constant {
     public static String        userID;
     public static String        userPassword;
     public static Set<String>   cookie;
-    public static String        userNewPassword;
     public static String        pushToken;
 
     /** Http 관련 상수 */
     public static final int     TIME_OUT_MILLIS = 2000;
     public static final String  DEFAULT_ENCODING = "UTF-8";
 
-    public static final String MESSAGE           = "message";
-    public static final String MESSAGE_SUCCESS   = "Success";
     public static final String SERVER_URL        = "http://52.78.113.6:8000";
     public static final int    SERVER_PORT       = 8000;
 }
